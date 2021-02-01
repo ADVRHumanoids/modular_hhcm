f0364cb767ce6c2325d5eb1dc7412d606873a1fe
m-tartari <michele.tartari@iit.it>
2021-02-01 12:22:42 +0100

    updated deploy.sh to use template and flags
    deploy.sh doesn't have files' content harcoded anymore, it now copies files form template situated in [path_to_modular]/src/modular/modular_data/ModularBot.
    deploy.sh now also support some flags:
    	-h/-H/--help for help,
    	-v/-V/--verbose for verbose output,
    	-d/-D/--destination-folder to specify the folder in which the ROS package will be deployed.

--- src/modular/modular_data/deploy.sh
+++ src/modular/modular_data/deploy.sh
@@ -7,354 +7,159 @@ ORANGE='\033[0;33m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-if [ "$#" -lt 1 ]; then
-       printf "${RED}No package name argument passed!${NC}"
-       echo
-       echo "Try something like:                         ./deply.sh modular_ros_pkg"
-       echo "you can also add a destination folder like: ./deply.sh modular_ros_pkg ~/catkin_ws/src"
-       exit
-fi
-package_name="$1"
+end_exec(){
+    # end execution cleanly regardless of the scripe being sourced or not
+    [[ "${BASH_SOURCE[0]}" != "${0}" ]] && return || exit 1
+}
 
-if [ $# -lt 2 ]; then
+print_help () {
+    printf "${ORANGE}"
+    echo "Try something like:                         ./deply.sh modular_ros_pkg"
+    echo "You can choose the destination folder with: -d /path/to/destination/dir"
+    echo "You can add vebosity with:                  -v"
+    printf "${NC}"
+    end_exec
+}
+
+if [ -n "$ROBOTOLOGY_ROOT" ]; then
     export DESTINATION_FOLDER=$ROBOTOLOGY_ROOT/robots
-else
-    export DESTINATION_FOLDER="$2"
 fi
-printf "Deploying package ${GREEN}${package_name}${NC} into ${RED}$DESTINATION_FOLDER${NC}"
-echo
+
+# read arguments
+while test $# -gt 0
+do
+    case "$1" in
+        -[vV] | --verbose) # add verbosity
+            VERBOSITY='-v'
+            printf "Verbose mode ${GREEN}ON${NC}\n"
+            ;;
+        -[dD] | --destination-folder) # add destination folder
+            shift
+            case "$1" in
+                -*)
+		            printf "${RED}Path ('$1')  is not valid!${NC}\n"
+       		        print_help
+                    ;;
+                *)  export DESTINATION_FOLDER=$1;
+                ;;
+            esac
+            ;;
+        -[hH] | --help) # Print suggestions
+            echo "Help incoming:";
+            print_help
+            ;;
+        -*) echo "bad option $1"
+	        print_help
+            ;;
+        *)  if [ -n "$package_name" ]; then
+		        printf "${RED}Package name inserted twice!${NC}\n"
+       		    print_help
+	        else
+		        package_name="$1"
+	        fi
+            ;;
+    esac
+    shift
+done
+
+# check correctness of input
+if ! [ -n "$package_name" ]; then
+	printf "${RED}No package name argument passed!${NC}\n"
+    print_help
+else
+    if ! [ -n "$DESTINATION_FOLDER" ]; then
+        printf "${RED}No destination fodler specified!${NC}\n"
+        print_help
+    else
+        printf "${GREEN}Deploying package ${YELLOW}${package_name}${GREEN} into ${YELLOW}$DESTINATION_FOLDER${NC}\n"
+    fi
+fi
+
+mkdir -p $DESTINATION_FOLDER/${package_name}
+pushd $DESTINATION_FOLDER/${package_name} > /dev/null #hide print
 
 # this way the script can be called from any directory
 SCRIPT_ROOT=$(dirname $(readlink --canonicalize --no-newline $BASH_SOURCE))
 
-cp -TRfv /tmp/modular $DESTINATION_FOLDER/${package_name}
+# Deploy ROS package info
+# - package.xml
+cp $SCRIPT_ROOT/ModularBot/package.xml ./package.xml $VERBOSITY || end_exec
+sed -i -e "s+[package_name]+${package_name}+g" ./package.xml
+# - CMakeLists.txt
+cp $SCRIPT_ROOT/ModularBot/CMakeLists.txt ./CMakeLists.txt $VERBOSITY || end_exec
+sed -i -e "s+[package_name]+${package_name}+g" ./CMakeLists.txt
+printf "${GREEN}[1/9] Deployed ROS package config files${NC}\n"
 
-cd $DESTINATION_FOLDER/${package_name}
+# Deploy Xbot2 configs
+# - Low level config
+mkdir -p ./configs
+cp /tmp/modular/configs/ModularBot.yaml ./configs/${package_name}.yaml $VERBOSITY || end_exec
+sed -i -e "s+ModularBot+${package_name}+g" ./configs/${package_name}.yaml
+# - High level config
+cp $SCRIPT_ROOT/ModularBot/ModularBot_basic.yaml ./${package_name}_basic.yaml $VERBOSITY || end_exec
+sed -i -e "s+[package_name]+${package_name}+g" ./${package_name}_basic.yaml
+printf "${GREEN}[2/9] Deployed XBot2 configs${NC}\n"
 
-cat > package.xml << EOF
-<package>
+# Deploy cartesio config
+mkdir -p ./cartesio
+# - cartesio.launch
+cp $SCRIPT_ROOT/ModularBot/cartesio/cartesio.launch ./cartesio/cartesio.launch $VERBOSITY || end_exec
+sed -i -e "s+[package_name]+${package_name}+g" ./cartesio/cartesio.launch
+# - /ModularBot_cartesio_config.yaml
+cp /tmp/modular/cartesio/ModularBot_cartesio_config.yaml ./cartesio/${package_name}_cartesio_config.yaml $VERBOSITY || end_exec
+sed -i -e "s+ModularBot+${package_name}+g" ./cartesio/${package_name}_cartesio_config.yaml
+printf "${GREEN}[3/9] Deployed cartesio configs${NC}\n"
 
-  <name>$package_name</name>
-  <version>1.0.0</version>
-  <description>
-  Modular robots tool
-  </description>
-  <maintainer email="edoardo.romiti@iit.it">Edoardo Romiti</maintainer>
-  <license>BSD</license>
+# Deploy launch files
+mkdir -p ./launch
+# - ModularBot_ik.launch
+cp $SCRIPT_ROOT/ModularBot/launch/ModularBot_ik.launch ./launch/${package_name}_ik.launch $VERBOSITY || end_exec
+sed -i -e "s+[package_name]+${package_name}+g" ./launch/${package_name}_ik.launch
+# - ModularBot_sliders.launch
+cp $SCRIPT_ROOT/ModularBot/launch/ModularBot_sliders.launch ./launch/${package_name}_sliders.launch $VERBOSITY || end_exec
+sed -i -e "s+[package_name]+${package_name}+g" ./launch/${package_name}_sliders.launch
+printf "${GREEN}[4/9] Deployed ${package_name}_ik.launch and ${package_name}_sliders.launch${NC}\n"
 
-  <buildtool_depend>catkin</buildtool_depend>
-  <build_depend>roslaunch</build_depend>
-  <run_depend>joint_state_publisher</run_depend>
-  <run_depend>robot_state_publisher</run_depend>
-  <run_depend>rviz</run_depend>
-  <run_depend>xacro</run_depend>
+# Deply meshes
+mkdir -p -p ./database/${package_name}_fixed_base
+cp -TRf $SCRIPT_ROOT/../web/static/models ./database $VERBOSITY || end_exec
+printf "${GREEN}[5/9] Deployed meshes${NC}\n"
 
-</package>
-EOF
+# Deploy joint_map
+mkdir -p ./joint_map
+cp /tmp/modular/joint_map/ModularBot_joint_map.yaml ./joint_map/${package_name}_joint_map.yaml $VERBOSITY || end_exec
+sed -i -e "s+ModularBot+${package_name}+g" ./joint_map/${package_name}_joint_map.yaml
+printf "${GREEN}[6/9] Deployed joint map${NC}\n"
 
-cat > CMakeLists.txt << EOF
-cmake_minimum_required(VERSION 2.8.3)
-project($package_name)
+# Deploy SRDF
+mkdir -p ./srdf
+cp /tmp/modular/srdf/ModularBot.srdf ./srdf/${package_name}.srdf $VERBOSITY || end_exec
+sed -i -e "s+ModularBot+${package_name}+g" ./srdf/${package_name}.srdf
+printf "${GREEN}[7/9] Deployed SRDF${NC}\n"
 
-find_package(catkin REQUIRED)
-catkin_package()
+# Deploy URDF
+mkdir -p ./urdf
+cp /tmp/modular/urdf/ModularBot.urdf ./urdf/${package_name}.urdf $VERBOSITY || end_exec
+sed -i -e "s+ModularBot+${package_name}+g" ./urdf/${package_name}.urdf
+printf "${GREEN}[8/9] Deployed URDF${NC}\n"
 
-if(CATKIN_ENABLE_TESTING)
-  find_package(roslaunch REQUIRED)
-  roslaunch_add_file_check(cartesio)
-endif()
-
-install(DIRECTORY urdf srdf launch
-  DESTINATION
-EOF
-
-cat >> CMakeLists.txt << 'EOF'
-${CATKIN_PACKAGE_SHARE_DESTINATION})
-EOF
-
-#cat > ${package_name}_basic.yaml << EOF
-
-mkdir cartesio
-cd cartesio
-cat > cartesio.launch << EOF
-<launch>
-    <arg name="pkg_name"  default="$package_name"/>
-EOF
-
-cat >> cartesio.launch << 'EOF'
-    <param name="robot_description" textfile="$(eval find(pkg_name) + '/urdf/ModularBot.urdf')"/>
-    <param name="robot_description_semantic" textfile="$(eval find(pkg_name) + '/srdf/ModularBot.srdf')"/>
-    <param name="cartesian/problem_description" textfile="$(eval find(pkg_name) + '/cartesio/stack.yaml')"/>
-
-    <arg name="solver" default="OpenSot"/>
-    <arg name="prefix" default=""/>
-    <arg name="use_xbot_config" default="false"/>
-    <arg name="verbosity" default="2"/>
-    <arg name="rate" default="500.0"/>
-    <arg name="tf_prefix" default="ci"/>
-    <arg name="markers" default="true"/>
-    <arg name="namespace" default=""/> <!-- dummy argument avoids pass_all_args error in parent launch file -->
-    <arg name="robot" default=""/>
-    <arg name="is_model_floating_base" default="true"/>
-
-
-    <node pkg="cartesian_interface" type="ros_server_node"
-                                    name="ros_server_node"
-                                    required="true"
-                                    output="screen"
-                                    launch-prefix="$(arg prefix)">
-
-        <param name="is_model_floating_base" value="$(arg is_model_floating_base)"/>
-        <param name="model_type" value="RBDL"/>
-        <param name="solver" value="$(arg solver)"/>
-        <param name="use_xbot_config" value="$(arg use_xbot_config)"/>
-        <param name="verbosity" value="$(arg verbosity)"/>
-        <param name="rate" value="$(arg rate)"/>
-        <param name="tf_prefix" value="$(arg tf_prefix)"/>
-
-    </node>
-
-    <node if="$(arg markers)" pkg="cartesian_interface" type="marker_spawner" name="interactive_markers" output="screen">
-        <param name="tf_prefix" value="$(arg tf_prefix)"/>
-    </node>
-</launch>
-EOF
-
-#cat > ${package_name}_cartesio.launch << EOF
-#<launch>
-#    <arg name="pkg_name"  default="${package_name}"/>
-#EOF
-#
-#cat >> ${package_name}_cartesio.launch << 'EOF'
-#    <arg name="rate" default="100.0"/>
-#    <arg name="prefix" default=""/>
-#    <param name="cartesian/problem_description"
-#        textfile="$(eval find(pkg_name) + '/cartesio/ModularBot_cartesio_config.yaml')"/>
-#
-#    <param name="is_model_floating_base" value="false"/>
-#</launch>
-#EOF
-
-cd ..
-
-mkdir launch
-cd launch
-
-printf "${GREEN}Creating ModularBot_ik.launch${NC}"
-echo
-
-cat > ModularBot_ik.launch << EOF
-<launch>
-    <arg name="pkg_name"  default="$package_name"/>
-EOF
-
-cat >> ModularBot_ik.launch << 'EOF'
-    <param name="robot_description" textfile="$(eval find(pkg_name) + '/urdf/ModularBot.urdf')"/>
-    <param name="robot_description_semantic" textfile="$(eval find(pkg_name) + '/srdf/ModularBot.srdf')"/>
-    <param name="cartesian/problem_description" textfile="$(eval find(pkg_name) + '/cartesio/ModularBot_cartesio_IK_config.yaml')"/>
-
-    <arg name="solver" default="OpenSot"/>
-    <arg name="prefix" default=""/>
-    <arg name="use_xbot_config" default="false"/>
-    <arg name="verbosity" default="0"/>
-    <arg name="rate" default="100.0"/>
-    <arg name="tf_prefix" default="ci"/>
-    <arg name="markers" default="true"/>
-    <arg name="namespace" default=""/>
-
-
-    <node pkg="cartesian_interface" type="ros_server_node"
-                                    name="ros_server_node"
-                                    output="screen"
-                                    launch-prefix="$(arg prefix)"
-                                    respawn="true">
-                                    <!-- required="true" -->
-
-        <param name="is_model_floating_base" type="bool" value="false"/>
-        <param name="model_type" value="RBDL"/>
-        <param name="solver" value="$(arg solver)"/>
-        <param name="use_xbot_config" value="$(arg use_xbot_config)"/>
-        <param name="verbosity" value="$(arg verbosity)"/>
-        <param name="rate" value="$(arg rate)"/>
-        <param name="tf_prefix" value="$(arg tf_prefix)"/>
-
-    </node>
-
-    <node if="$(arg markers)" pkg="cartesian_interface" type="marker_spawner" name="interactive_markers" output="screen">
-        <param name="tf_prefix" value="$(arg tf_prefix)"/>
-    </node>
-
-    <!-- <node name="joint_state_publisher" pkg="joint_state_publisher" type="joint_state_publisher">
-        <param name="use_gui" value="true"/>
-        <param name="rate" value="10"/>
-        <remap from="joint_states" to="cartesian/posture/reference"/>
-        <remap from="zeros" to="cartesian/posture/home"/>
-    </node> -->
-
-</launch>
-
-EOF
-
-cat > ModularBot_sliders.launch << EOF
-<launch>
-    <arg name="gui" default="true" />
-    <arg name="pkg_name"  default="$package_name"/>
-EOF
-
-cat >> ModularBot_sliders.launch << 'EOF'
-    <param name="robot_description" textfile="$(eval find(pkg_name) + '/urdf/ModularBot.urdf')"/>
-    <param name="robot_description_semantic" textfile="$(eval find(pkg_name) + '/srdf/ModularBot.srdf')"/>
-
-    <param name="use_gui" value="$(arg gui)"/>
-    <param name="rate" value="50.0"/>
-
-
-    <node name="joint_state_publisher" pkg="joint_state_publisher" type="joint_state_publisher">
-    <param name="publish_default_efforts" value="True"/>
-    </node>
-
-    <!-- start robot state publisher -->
-    <node pkg="robot_state_publisher" type="robot_state_publisher" name="robot_state_publisher" output="screen" >
-        <param name="publish_frequency" type="double" value="250.0" />
-    </node>
-
-</launch>
-EOF
-cd ..
-
-mkdir -p $DESTINATION_FOLDER/${package_name}/database/ModularBot_fixed_base
-# cd $SCRIPT_ROOT/../web/static/
-cp -TRfv $SCRIPT_ROOT/../web/static/models $DESTINATION_FOLDER/${package_name}/database
-
-#cd $DESTINATION_FOLDER/${package_name}
-cd ./database/ModularBot_fixed_base
-cat > manifest.xml << EOF
-<?xml version="1.0"?>
-<model>
-  <name>${package_name}</name>
-  <version>0.1</version>
-  <sdf version='1.6'>${package_name}.sdf</sdf>
-
-  <author>
-   <name>Edoardo Romiti</name>
-   <email>edoardo.romiti@iit.it</email>
-  </author>
-
-  <description>
-    Simulation of the Alberobotics's ${package_name}.
-  </description>
-</model>
-
-EOF
-
-cat > model.config << EOF
-<?xml version="1.0"?>
-<model>
-  <name>${package_name}</name>
-  <version>0.1</version>
-  <sdf version='1.6'>${package_name}.sdf</sdf>
-
-  <author>
-   <name>Edoardo Romiti</name>
-   <email>edoardo.romiti@iit.it</email>
-  </author>
-
-  <description>
-    Simulation of the Alberobotics's ${package_name}.
-  </description>
-</model>
-
-EOF
-
-cat > modular_world.sdf << EOF
-<sdf version='1.6'>
-  <world name='default'>
-  <physics type="ode">
-    <max_step_size>0.0001</max_step_size>
-    <real_time_factor>1</real_time_factor>
-    <real_time_update_rate>10000</real_time_update_rate>
-    <ode>
-      <solver>
-        <type>quick</type>
-        <min_step_size>0.00001</min_step_size>
-        <iters>70</iters>
-        <sor>1.3</sor>
-        <use_dynamic_moi_rescaling>0</use_dynamic_moi_rescaling>
-      </solver>
-      <constraints>
-        <cfm>0</cfm>
-        <erp>0.2</erp>
-        <contact_surface_layer>0</contact_surface_layer>
-        <contact_max_correcting_vel>100</contact_max_correcting_vel>
-      </constraints>
-    </ode>
-  </physics>
-  <gravity>0.0 0.0 -9.81</gravity>
-    <light name='sun' type='directional'>
-      <cast_shadows>1</cast_shadows>
-      <pose frame=''>0 0 10 0 -0 0</pose>
-      <diffuse>0.8 0.8 0.8 1</diffuse>
-      <specular>0.2 0.2 0.2 1</specular>
-      <attenuation>
-        <range>1000</range>
-        <constant>0.9</constant>
-        <linear>0.01</linear>
-        <quadratic>0.001</quadratic>
-      </attenuation>
-      <direction>-0.5 0.1 -0.9</direction>
-    </light>
-    <model name='ground_plane'>
-      <static>1</static>
-      <link name='link'>
-        <visual name='visual'>
-          <cast_shadows>0</cast_shadows>
-          <geometry>
-            <plane>
-              <normal>0 0 1</normal>
-              <size>100 100</size>
-            </plane>
-          </geometry>
-          <material>
-            <script>
-              <uri>file://media/materials/scripts/gazebo.material</uri>
-              <name>Gazebo/Grey</name>
-            </script>
-          </material>
-        </visual>
-        <self_collide>0</self_collide>
-        <enable_wind>0</enable_wind>
-        <kinematic>0</kinematic>
-      </link>
-    </model>
-    <magnetic_field>6e-06 2.3e-05 -4.2e-05</magnetic_field>
-    <atmosphere type='adiabatic'/>
-    <scene>
-      <ambient>0.4 0.4 0.4 1</ambient>
-      <background>0.7 0.7 0.7 1</background>
-      <shadows>1</shadows>
-    </scene>
-    <wind/>
-    <spherical_coordinates>
-      <surface_model>EARTH_WGS84</surface_model>
-      <latitude_deg>0</latitude_deg>
-      <longitude_deg>0</longitude_deg>
-      <elevation>0</elevation>
-      <heading_deg>0</heading_deg>
-    </spherical_coordinates>
-    <gui fullscreen='0'>
-      <camera name='user_camera'>
-        <pose frame=''>5 -5 2 0 0.275643 2.35619</pose>
-        <view_controller>orbit</view_controller>
-        <projection_type>perspective</projection_type>
-      </camera>
-    </gui>
-  </world>
-</sdf>
-
-EOF
-
+# Deploy gazebo model
+# - modular_world.sdf
+cp $SCRIPT_ROOT/ModularBot/database/ModularBot_fixed_base/ModularBot_world.sdf ./database/${package_name}_fixed_base/${package_name}_world.sdf $VERBOSITY || end_exec
+# - manifest.xml
+cp $SCRIPT_ROOT/ModularBot/database/ModularBot_fixed_base/manifest.xml ./database/${package_name}_fixed_base/manifest.xml $VERBOSITY || end_exec
+sed -i -e "s+[package_name]+${package_name}+g" ./database/${package_name}_fixed_base/manifest.xml
+# - model.config
+cp $SCRIPT_ROOT/ModularBot/database/ModularBot_fixed_base/model.config ./database/${package_name}_fixed_base/model.config $VERBOSITY || end_exec
+sed -i -e "s+[package_name]+${package_name}+g" ./database/${package_name}_fixed_base/model.config
+# - ModularBot.sdf
 gz sdf --print \
-    $DESTINATION_FOLDER/${package_name}/urdf/ModularBot.urdf > \
-    $DESTINATION_FOLDER/${package_name}/database/ModularBot_fixed_base/${package_name}.sdf
+    $DESTINATION_FOLDER/${package_name}/urdf/${package_name}.urdf > \
+    $DESTINATION_FOLDER/${package_name}/database/${package_name}_fixed_base/${package_name}.sdf \
+    || end_exec
+printf "${GREEN}[9/9] Deployed gazebo model${NC}\n"
 
-printf "\n\n${GREEN}Package ${package_name} succesfully deployed into $DESTINATION_FOLDER${NC}\n\n"
+# All done
+popd > /dev/null #hide print
+printf "\n${GREEN}[ \xE2\x9C\x94 ] Package ${YELLOW}${package_name}${GREEN} succesfully deployed into ${YELLOW}$DESTINATION_FOLDER${NC}\n\n"
+unset DESTINATION_FOLDER VERBOSITY SCRIPT_ROOT RED PURPLE GREEN ORANGE YELLOW NC package_name
