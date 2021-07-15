4da81c61e20e055b7bd6fc5cd9593eef1b524018
Edoardo Romiti <edoardo.romiti@iit.it>
2021-07-15 17:35:21 +0200

    fix : fix wrong meshes orientation

--- src/modular/web/static/URDF_viewer.js
+++ src/modular/web/static/URDF_viewer.js
@@ -1474,7 +1474,7 @@ class URDF_viewer extends HTMLElement {
 
                                 mesh.position.set(xyz[0], xyz[1], xyz[2])
                                 //TODO: to be changed! the two minus probably work only for the latest joint elbow mesh!
-                                mesh.rotation.set(rpy[0], -rpy[1], -rpy[2])
+                                mesh.rotation.set(rpy[0], rpy[1], rpy[2])
 
                                 mesh.scale.set(scale[0], scale[1], scale[2])
 
