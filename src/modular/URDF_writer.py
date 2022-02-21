24f80e8874f8dd9c675ab606c0c52d16811bba31
EdoardoRomiti <edoardo.romiti@iit.it>
2022-02-21 19:23:49 +0100

    fix (xbot2): config for idle mode

--- src/modular/URDF_writer.py
+++ src/modular/URDF_writer.py
@@ -833,6 +833,13 @@ class XBot2Plugin(Plugin):
                     del impd4_joint_config[key].pos_offset 
                     del impd4_joint_config[key].max_current_A 
 
+                    idle_joint_config[key] = copy.deepcopy(value)
+                    idle_joint_config[key].control_mode = 'idle'
+                    # Remove parameters that are now not used by XBot2 (they are handled by the EtherCat master on a different config file)   
+                    del idle_joint_config[key].sign 
+                    del idle_joint_config[key].pos_offset 
+                    del idle_joint_config[key].max_current_A 
+
                     # HACK: Every joint on 2nd, 3rd, etc. chains have the torque loop damping set very low.
                     # This is to handle chains with only one joint and low inertia after it.
                     # If we build two big robots this could have catastrophic effects
@@ -849,12 +856,10 @@ class XBot2Plugin(Plugin):
                     value = joint_module.LpESC
                     impd4_joint_config[key] = copy.deepcopy(value)
                     impd4_joint_config[key].control_mode = '3B_motor_pos_ctrl'
+                    # Remove parameters that are now not used by XBot2 (they are handled by the EtherCat master on a different config file)   
                     del impd4_joint_config[key].sign 
                     del impd4_joint_config[key].pos_offset 
                     del impd4_joint_config[key].max_current_A 
-                    
-                elif joint_module.type == 'simple_ee':
-                    continue
 
                 idle_joint_config[key] = copy.deepcopy(value)
                 idle_joint_config[key].control_mode = 'idle'
@@ -862,6 +867,16 @@ class XBot2Plugin(Plugin):
                 del idle_joint_config[key].sign 
                 del idle_joint_config[key].pos_offset 
                 del idle_joint_config[key].max_current_A 
+
+                elif joint_module.type == 'simple_ee':
+                    continue
+
+                # idle_joint_config[key] = copy.deepcopy(value)
+                # idle_joint_config[key].control_mode = 'idle'
+                # Remove parameters that are now not used by XBot2 (they are handled by the EtherCat master on a different config file)
+                # del idle_joint_config[key].sign 
+                # del idle_joint_config[key].pos_offset 
+                # del idle_joint_config[key].max_current_A 
 
         # Create folder if doesen't exist
         if not os.path.exists(os.path.dirname(idle_joint_config_filename)):
