b47830d94f3fe6971ac6789f079924f62bba5213
EdoardoRomiti <edoardo.romiti@iit.it>
2022-11-21 15:06:10 +0100

    move `modular_resources` (yaml, json, meshes) out of `modular_data`

--- src/modular/web/RobotDesignStudio.py
+++ src/modular/web/RobotDesignStudio.py
@@ -289,7 +289,7 @@ def requestURDF():
 # This is needed to load the meshes of the modules (withot the need to put them in the /static folder)
 @app.route('/modular_resources/<path:path>')
 def send_file(path):
-    return send_from_directory('/home/eromiti/modular/src/modular/modular_data/modular_resources', path)
+    return send_from_directory('/home/eromiti/modular/src/modular/modular_resources', path)
 
 
 #TODO: to be included in the next versions (requires ROS etc.)
