2c6e022a35c75160f7481dac37429de05f1209df
Arturo Laurenzi <arturo.laurenzi@iit.it>
2022-12-22 18:51:07 +0100

    fixed modular_path in xacro to be compatible with pip install

--- src/modular/URDF_writer.py
+++ src/modular/URDF_writer.py
@@ -1016,7 +1016,7 @@ class UrdfWriter:
         self.parent = parent
 
         # xacro mappings to perform args substitution (see template.urdf.xacro)
-        self.default_xacro_mappings = {'modular_path': os.path.dirname(os.path.realpath(__file__))+'/../..',
+        self.default_xacro_mappings = {'modular_path': os.path.dirname(os.path.realpath(__file__)),
                                 'floating_base': 'false',
                                 'gazebo_urdf': 'false',
                                 'velodyne': 'false',
