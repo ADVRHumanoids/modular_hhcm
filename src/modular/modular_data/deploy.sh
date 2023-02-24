c6d5b4477f1fe70691a067ab170e237bce9c26de
EdoardoRomiti <edoardo.romiti@iit.it>
2023-02-24 15:55:28 +0100

    remove deployment of gazebo model

--- src/modular/modular_data/deploy.sh
+++ src/modular/modular_data/deploy.sh
@@ -183,9 +183,9 @@ printf "${GREEN}[4/9] Deployed ModularBot launch files${NC}\n"
 #cp -TRf $SCRIPT_ROOT/moveit_launch ./launch $VERBOSITY || end_exec
 #printf "${GREEN}[4.5/9] Deployed moveit configs and launch files${NC}\n"
 
-mkdir -p -p ./database/${package_name}_fixed_base
-# Deply meshes 
-#NOTE: currently meshes are not deployed and instead taken from the modular_resources or concert_resources packages
+# # Deply meshes 
+# #NOTE: currently meshes are not deployed and instead taken from the modular_resources or concert_resources packages
+# mkdir -p -p ./database/${package_name}_fixed_base
 # cp -TRf $SCRIPT_ROOT/../modular_resources/models ./database $VERBOSITY || end_exec
 # printf "${GREEN}[5/9] Deployed meshes${NC}\n"
 
@@ -214,21 +214,21 @@ sed -i -e "s+/tmp/ModularBot+package://${package_name}+g" ./urdf/ModularBot.gaze
 # sed -i -e "s+package://modular/src/modular/modular_resources/models/modular/meshes+package://${package_name}/database/modular/meshes+g" ./urdf/ModularBot.gazebo.urdf
 printf "${GREEN}[8/9] Deployed URDF${NC}\n"
 
-# Deploy gazebo model
-# - modular_world.sdf
-cp $SCRIPT_ROOT/database/ModularBot_fixed_base/ModularBot_world.sdf ./database/${package_name}_fixed_base/${package_name}_world.sdf $VERBOSITY || end_exec
-# - manifest.xml
-cp $SCRIPT_ROOT/database/ModularBot_fixed_base/manifest.xml ./database/${package_name}_fixed_base/manifest.xml $VERBOSITY || end_exec
-sed -i -e "s+PACKAGE_NAME+${package_name}+g" ./database/${package_name}_fixed_base/manifest.xml
-# - model.config
-cp $SCRIPT_ROOT/database/ModularBot_fixed_base/model.config ./database/${package_name}_fixed_base/model.config $VERBOSITY || end_exec
-sed -i -e "s+PACKAGE_NAME+${package_name}+g" ./database/${package_name}_fixed_base/model.config
-# # - ModularBot.sdf
-# gz sdf --print \
-#     $DESTINATION_FOLDER/${package_name}/urdf/ModularBot.urdf > \
-#     $DESTINATION_FOLDER/${package_name}/database/${package_name}_fixed_base/${package_name}.sdf \
-#     || end_exec
-printf "${GREEN}[9/9] Deployed gazebo model${NC}\n"
+# # Deploy gazebo model
+# # - modular_world.sdf
+# cp $SCRIPT_ROOT/database/ModularBot_fixed_base/ModularBot_world.sdf ./database/${package_name}_fixed_base/${package_name}_world.sdf $VERBOSITY || end_exec
+# # - manifest.xml
+# cp $SCRIPT_ROOT/database/ModularBot_fixed_base/manifest.xml ./database/${package_name}_fixed_base/manifest.xml $VERBOSITY || end_exec
+# sed -i -e "s+PACKAGE_NAME+${package_name}+g" ./database/${package_name}_fixed_base/manifest.xml
+# # - model.config
+# cp $SCRIPT_ROOT/database/ModularBot_fixed_base/model.config ./database/${package_name}_fixed_base/model.config $VERBOSITY || end_exec
+# sed -i -e "s+PACKAGE_NAME+${package_name}+g" ./database/${package_name}_fixed_base/model.config
+# # # - ModularBot.sdf
+# # gz sdf --print \
+# #     $DESTINATION_FOLDER/${package_name}/urdf/ModularBot.urdf > \
+# #     $DESTINATION_FOLDER/${package_name}/database/${package_name}_fixed_base/${package_name}.sdf \
+# #     || end_exec
+# printf "${GREEN}[9/9] Deployed gazebo model${NC}\n"
 
 # All done
 popd > /dev/null #hide print
